b26b08350221388298ac29570a2befa52485a93e
David McGarvey <davidjmcgarvey@gmail.com>
2019-07-23 12:03:54 -0700

    add showPage function

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ Full Stack JavaScript Techdegree Project 2
 
 Welcome to my list pagination project!
 
+*BASIC FEATURES*
 The JavaScript file in this directory will enhance the HTML provided by paginating lists of students into crisp clean sets of 10 per page. Using the core principles of unobtrusive JavaScript, this app will be able to take HTML with any number of students, and display the appropriate number of students per page as well as links to all accompanying pages.
 
+*EXTRA FEATURES*
 These is also search functionality built into this app. Simple search a name in the provided search field to yield desired results. Enjoy!
 
 David McGarvey 2019-07-23
--- js/script.js
+++ js/script.js
@@ -1,53 +1,33 @@
 /******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
+Full Stack JavaScript Techdegree Project 2
+-----------------------------------------
+##    List Pagination and Filtering    ##
+-----------------------------------------
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+// Global variables
+const listItem = document.querySelectorAll('.student-item');
+const page = 10;
 
 
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
-const list;
-const page;
-
-
-
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
-
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
-function showPage() {
-  
+// Hides all but first 10 students in list
+function showPage(list, page) {
+  for (let i = 0;i < list.length;i++) {
+    if (i < 10) {
+      const shownItems = list[i].style.display = 'block';
+    } else {
+      const hiddenItems = list[i].style.display = 'none';
+    }
+  }
+  return shownItems;
 }
 
 
+// Generates, appends, and adds funcitonality to pagination buttons
+function appendPageLinks() {
 
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
-
+}
 
 
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// Function calls
+showPage(listItem, page);
