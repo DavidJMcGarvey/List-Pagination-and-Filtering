e353f640e642a212e9815e7dd4984f19cd1706fd
David McGarvey <davidjmcgarvey@gmail.com>
2019-07-26 13:08:33 -0700

    progress-search function

--- js/script.js
+++ js/script.js
@@ -59,12 +59,55 @@ const pageHeader = document.querySelector('.page-header');
 const searchDiv = document.createElement('div');
 const searchInput = document.createElement('input');
 const searchButton = document.createElement('button');
+
+const searchNames = document.querySelectorAll('li');
+const searchList = [];
+
 searchDiv.className = 'student-search';
 searchInput.placeholder = 'Search for students...';
 searchButton.textContent = 'Search';
 searchDiv.appendChild(searchInput);
 searchDiv.appendChild(searchButton);
 pageHeader.appendChild(searchDiv);
+
+// Search functionality
+function searchFunction(input, names) {
+  for (let i = 0; i < names.length; i++) {
+    if (input.value.length !== 0 && names[i].textContent.toLowerCase().includes(input.value)) {
+      names[i].className = 'student-item cf match';
+    }
+  }
+}
+
+// Event listener for search button
+searchButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  searchFunction(searchInput, searchNames);
+  const matches = document.querySelectorAll('.match')
+  showPage(matches, 1);
+});
+
+// NOT SURE
+searchInput.addEventListener('keyup',  () => {
+  searchFunction(searchInput, searchNames);
+  console.log('Keyup event on the Search input is functional!');
+});
+
+
+// TEST - keyup
+// searchInput.addEventListener('keyup', (e) => {
+//   searchInput.textContent += ' ${e.code}';
+//   for (let i = 0; i < names.length; i++ ) {
+//     if (searchInput.value.includes(names[i].textContent)) {
+//       searchButton.addEventListener('click', () => {
+//         searchList.push(names[i].textContent);
+//       });
+//     }
+//   }
+//   console.log();
+// });
+
+
 
 // Function calls
 showPage(listItems, 1);
