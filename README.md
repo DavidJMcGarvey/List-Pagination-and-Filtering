7bf2b6998ef6d3b31bba89ee1fc57315ea4c9d7b
David McGarvey <davidjmcgarvey@gmail.com>
2019-07-29 18:18:39 -0700

    handle NO results returned, final cleanup before submit

--- README.md
+++ README.md
@@ -9,6 +9,6 @@ Welcome to my list pagination project!
 The JavaScript file in this directory will enhance the HTML provided by paginating lists of students into crisp clean sets of 10 per page. Using the core principles of unobtrusive JavaScript, this app will be able to take HTML with any number of students, and display the appropriate number of students per page as well as generate and append links to all accompanying pages.
 
 **EXTRA FEATURES**
-These is also search functionality built into this app. Search first or last names as well as characters that may be contained in a student name. Search results will first be presented in totality, with pages being available at the bottom to sort through search results.
-
-David J McGarvey 2019-07-27
+These is also search functionality built into this app. A search input field and button and dynamically added with JavaScript. Search first or last names as well as characters that may be contained in a student name. Search results will be paginated as well for better flow.
+ 
+David J McGarvey 2019-07-29
--- js/script.js
+++ js/script.js
@@ -7,20 +7,23 @@ Full Stack JavaScript Techdegree Project 2
 
 // Global variables
 const listItems = document.querySelectorAll('.student-item');
+const messageDiv = document.createElement('div');
 const pageItems = 10;
 const pageHeader = document.querySelector('.page-header');
+const pageDiv = document.querySelector('.page');
 const searchDiv = document.createElement('div');
 const searchInput = document.createElement('input');
 const searchButton = document.createElement('button');
-const searchList = [];
+const searchArray = [];
 
-// Hover over "Students" heading to see message on how to refresh student list
-const refreshList = document.querySelector('h2');
-refreshList.innerHTML = '<h2><span title="Click this to refresh student list">Students</span></h2>';
-refreshList.addEventListener('click', () => {
-  showPage(listItems, 1);
-  appendPageLinks(listItems);
-});
+// Remove pagination links so others can replace them
+function removePageLinks() {
+  // messageDiv.parentNode.removeChild(messageDiv);
+  const listDiv = document.querySelectorAll('div.pagination, div.list');
+  for (let i = 0; i < listDiv.length; i++) {
+    listDiv[i].parentNode.removeChild(listDiv[i]);
+  }
+}
 
 // Hide all but 10 students between START and END indexes
 function showPage(list, page) {
@@ -37,7 +40,6 @@ function showPage(list, page) {
 
 // Generate, append, and add funcitonality to pagination links
 function appendPageLinks(list) {
-  const pageDiv = document.querySelector('.page');
   const pages = Math.round(Math.ceil(list.length/pageItems));
   const div = document.createElement('div');
   const ul = document.createElement('ul');
@@ -67,25 +69,41 @@ function appendPageLinks(list) {
   }
 }
 
-// Search functionality, adds class of 'match' to matching students
+// Search functionality, adds class of 'match' to matching students, also handles no results
 function searchFunction(input, names) {
-  const listDiv = document.querySelectorAll('div.pagination, div.list');
-  for (let i = 0; i < listDiv.length; i++) {
-    listDiv[i].parentNode.removeChild(listDiv[i]);
+  const searchArray = [];
+  removePageLinks();
+  function noResults() {
+    const div = document.createElement('div');
+    const ul = document.createElement('ul');
+    const pageLi = document.createElement('li');
+    const pageLink = document.createElement('a');
+    div.className = 'pagination list';
+    pageLink.innerHTML = "No search results. Click here to refresh student list.";;
+    pageLink.href = 'javascript:window.location.reload(true)';
+    pageDiv.appendChild(div);
+    div.appendChild(ul);
+    ul.appendChild(pageLi);
+    pageLi.appendChild(pageLink);
   }
   for (let i = 0; i < names.length; i++) {
     names[i].className = 'student-item cf';
     names[i].style.display = 'none';
     if (input.value.length !== 0 && names[i].textContent.toLowerCase().includes(input.value)) {
       names[i].className = 'student-item cf match';
-      searchList.push(names[i]);
+      searchArray.push(names[i]);
       if (names[i].className !== 'student-item cf match') {
-        searchList.pop(names[i]);
+        searchArray.pop(names[i]);
       }
     }
   }
-  showPage(searchList, 1);
-  appendPageLinks(searchList);
+  if (searchArray.length === 0) {
+    noResults();
+  } else {
+    showPage(searchArray, 1);
+    appendPageLinks(searchArray);
+  }
+
 }
 
 // Classify search components
@@ -106,83 +124,7 @@ searchButton.addEventListener('click', (e) => {
 showPage(listItems, 1);
 appendPageLinks(listItems);
 
-// OLD - Search Functions - Not Great
-// // Shows first page of search results
-// function showSearchPage(list, page) {
-//   const indexStart = (page * pageItems) - pageItems;
-//   const indexEnd = page * pageItems;
-//   const matches = document.getElementsByClassName('student-item cf match');
-//   const nonMatches = document.getElementsByClassName('student-item cf');
-//   for (let i = 0; i <= list.length - 1 ; i++) {
-//     const student = list[i];
-//     if (student.className === 'student-item cf match') {
-//       appendSearchPageLinks(matches);
-//       const searchDiv = document.querySelector('.searchButton');
-//       const searchMessage = document.createElement('div');
-//       searchMessage.innerHTML = '<h4>Search Pages For -->  ' + searchInput.value + '</h4>';
-//       searchMessage.className = 'pagination';
-//       searchDiv.appendChild(searchMessage);
-//
-//       const listDiv = document.querySelector('.list');
-//       const listMessage = document.createElement('div');
-//       listMessage.innerHTML = '<h4>Pages For Entire Student Catalog</h4>';
-//       listMessage.className = 'pagination';
-//       listDiv.appendChild(listMessage);
-//
-//       if (i >= indexStart && i < indexEnd) {
-//         student.style.display = 'block';
-//       }
-//     } else if (matches.length === 0) {
-//       const message = document.createElement('div')
-//       message.innerHTML = '<h3>--> Search Yielded No Results</h3>';
-//       pageHeader.appendChild(message);
-//       break;
-//     } else {
-//       student.style.display = 'none';
-//     }
-//   }
-// }
-//
-// // Creates pagination for search results
-// function appendSearchPageLinks(list) {
-//   const oldDiv = document.querySelectorAll('.searchButton');
-//   for (let i = 0; i < oldDiv.length; i++) {
-//     oldDiv[i].parentNode.removeChild(oldDiv[i]);
-//   }
-//   const listDiv = document.getElementsByTagName('h4');
-//   for (let i = 0; i < listDiv.length; i++) {
-//     listDiv[i].parentNode.removeChild(listDiv[i]);
-//   }
-//   const pageDiv = document.querySelector('.page');
-//   const pages = Math.round(Math.ceil(list.length/pageItems));
-//   const div = document.createElement('div');
-//   const ul = document.createElement('ul');
-//   div.className = 'pagination searchButton';
-//   pageDiv.appendChild(div);
-//   for (let i = 1; i < pages + 1; i++) {
-//     const pageLi = document.createElement('li');
-//     const pageLink = document.createElement('a');
-//     pageLi.appendChild(pageLink);
-//     ul.appendChild(pageLi);
-//     pageLink.textContent = i;
-//     pageLink.href = '#';
-//     div.appendChild(ul);
-//     const current = document.querySelectorAll('a');
-//     current[0].className = 'active';
-//     pageLink.addEventListener('click', (e) => {
-//       showPage(list, i);
-//       const current = document.querySelectorAll('a');
-//       for (let i = 0; i < current.length; i++) {
-//         e.target.className = 'active';
-//         if (e.target.className === 'active') {
-//           current[i].className = current[i].className.replace('active', '');
-//           e.target.className = 'active';
-//         }
-//       }
-//     });
-//   }
-// }
 
 // Author: David J McGarvey
 // Date Created: 2019-07-23
-// Date Updated: 2019-07-27
+// Date Updated: 2019-07-29
